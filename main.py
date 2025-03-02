5fc99d71290c28a044c77e196ac71fd44aacc512
Fernando <fdelfedele@gmail.com>
2025-03-02 01:36:39 -0300

    get typesense working

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database.models import Car
+import typesense
 
 app = FastAPI()
 
@@ -64,3 +65,45 @@ def test_direct_connection():
     db_version = cursor.fetchone()[0]
     connection.close()
     return {f"Version is {db_version}"}
+
+
+@app.get("/testVectorDB")
+def test_direct_connection():
+    client = typesense.Client(
+        {
+            "nodes": [
+                {
+                    "host": "localhost",  # For Typesense Cloud use xxx.a1.typesense.net
+                    "port": "8108",  # For Typesense Cloud use 443
+                    "protocol": "http",  # For Typesense Cloud use https
+                }
+            ],
+            "api_key": "xyz",
+            "connection_timeout_seconds": 2,
+        }
+    )
+
+    books_schema = {
+        "name": "books",
+        "fields": [
+            {"name": "title", "type": "string"},
+            {"name": "authors", "type": "string[]", "facet": True},
+            {"name": "publication_year", "type": "int32", "facet": True},
+            {"name": "ratings_count", "type": "int32"},
+            {"name": "average_rating", "type": "float"},
+        ],
+        "default_sorting_field": "ratings_count",
+    }
+
+    # client.collections.create(books_schema)
+
+    # with open("typesense-data/books.jsonl") as jsonl_file:
+    #     client.collections["books"].documents.import_(jsonl_file.read().encode("utf-8"))
+    # return
+    search_parameters = {
+        "q": "harry potter",
+        "query_by": "title",
+        "sort_by": "ratings_count:desc",
+    }
+
+    return client.collections["books"].documents.search(search_parameters)
